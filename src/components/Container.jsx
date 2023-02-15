c8900d4a2e9774612ed285a33d080ef003b84684
MadeInUtrecht <74437330+MadeInUtrecht@users.noreply.github.com>
2023-02-15 11:53:22 +0100

    Overlay added

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { IoCloudDownloadOutline } from 'react-icons/io5';
+import { AiOutlineClose } from 'react-icons/ai';
 
 const styles = {
   container: `grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 display-flex justify-content-center align-items-center z-0 transition-all duration-500`,
   img: `rounded-lg shadow-lg display-flex justify-content-center align-items-center cursor-pointer hover: hover:scale-105 transition-all duration-500`,
-  popup: `w-[50%] bg-black bg-opacity-75 z-50 display-flex justify-content-center align-items-center rounded-lg fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2`,
+  popup: `w-[50%] bg-black bg-opacity-75 z-40 display-flex justify-content-center align-items-center rounded-lg fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2`,
   bg: `bg-black bg-opacity-75 z-50 fixed top-0 left-0 w-full h-full`,
+  overlay: `absolute top-1/2 left-1/2 p-11 bg-white bg-opacity-50 z-50 flex justify-center items-center rounded-full transform -translate-x-1/2 -translate-y-1/2 cursor-pointer`,
+  close: `absolute top-5 left-5 w-10 h-10 flex justify-center items-center cursor-pointer text-white`,
 
 };
 
@@ -42,6 +46,11 @@ const Container = () => {
     }
   }, []);
 
+  const handleDownload = (e) => {
+    e.stopPropagation();
+    window.open(clickedImage.src.original, '_blank', 'noopener');
+  };
+
   return (
     <div className={styles.container}>
       {images.map((image, index) => (
@@ -55,8 +64,19 @@ const Container = () => {
         </div>
       ))}
       {clickedImage && (
-        <div className={styles.bg} onClick={() => setClickedImage(null)}>
+        <div className={styles.bg}>
+          <div className={styles.close} onClick={() => setClickedImage(null)}>
+            <AiOutlineClose size={32} />
+          </div>
           <img src={clickedImage.src.original} alt={clickedImage.alt_description} className={styles.popup} />
+          <div
+            download={`${clickedImage.id}.jpg`}
+            className={styles.overlay}
+            title='Download 4K Image'
+            onClick={handleDownload}
+          >
+            <IoCloudDownloadOutline size={64} />
+          </div>
         </div>
       )}
     </div>
