5ce51ba4930d3f956709374b1e882114266dab08
MadeInUtrecht <74437330+MadeInUtrecht@users.noreply.github.com>
2023-02-15 15:10:40 +0100

    Resizing

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -8,7 +8,7 @@ const styles = {
   img: `rounded-lg shadow-lg display-flex justify-content-center align-items-center cursor-pointer hover: hover:scale-105 transition-all duration-500`,
   popup: `w-[50%] bg-black bg-opacity-75 z-40 display-flex justify-content-center align-items-center rounded-lg fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2`,
   bg: `bg-black bg-opacity-75 z-50 fixed top-0 left-0 w-full h-full`,
-  overlay: `absolute top-1/2 left-1/2 p-11 bg-white bg-opacity-50 z-50 flex justify-center items-center rounded-full transform -translate-x-1/2 -translate-y-1/2 cursor-pointer`,
+  overlay: `absolute top-1/2 left-1/2 p-6 bg-white bg-opacity-50 z-50 flex justify-center items-center rounded-full transform -translate-x-1/2 -translate-y-1/2 cursor-pointer hover:bg-opacity-75 transition-all duration-250`,
   close: `absolute top-5 left-5 w-10 h-10 flex justify-center items-center cursor-pointer text-white`,
 
 };
@@ -57,9 +57,6 @@ const Container = () => {
         <div key={index} className={styles.item} style={{ height: `${image.height.medium}px`, position: `relative` }}>
           <img src={image.src.medium} alt={image.alt_description} className={styles.img} onClick={() => setClickedImage(image)} />
           <div className={styles.overlay} style={{ display: `none` }}>
-            <div>
-              <p className={`text-white text-3xl`}>Download</p>
-            </div>
           </div>
         </div>
       ))}
@@ -68,7 +65,7 @@ const Container = () => {
           <div className={styles.close} onClick={() => setClickedImage(null)}>
             <AiOutlineClose size={32} />
           </div>
-          <img src={clickedImage.src.original} alt={clickedImage.alt_description} className={styles.popup} />
+          <img src={clickedImage.src.original} alt={clickedImage.alt_description} className={styles.popup}/>
           <div
             download={`${clickedImage.id}.jpg`}
             className={styles.overlay}
